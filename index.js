0a07f1b82e54958107f0201553cfc1e3ff8d21a0
docanam <daniel.ocana@hotmail.com>
2021-04-01 21:55:24 +0200

    2 files modified

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import './styles.css';
 var visualization = d3plus
   .viz()
   .container("#viz1")
-  .data([{"year": 1991, "name":"alpha", "value": 15}, {"year": 1992, "name":"alpha", "value": 20},])
+  .data([{"year": 1991, "name":"alpha", "value": 15},
+  {"year": 1992, "name":"alpha", "value": 20},])
   .type("bar")
   .id("name")
   .x("year")
